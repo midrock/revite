33d6630fdd613a4a8750331526126f498fcbba92
Roman Tebenkov <iteam4u@ya.ru>
2022-02-08 03:38:58 +0300

    style: update analyzer

--- release.config.js
+++ release.config.js
@@ -4,7 +4,14 @@ module.exports = {
   ],
   ci: false,
   plugins: [
-    '@semantic-release/commit-analyzer',
+    ['@semantic-release/commit-analyzer', {
+      preset: 'angular',
+      releaseRules: [
+        { type: 'docs', scope: 'README', release: 'patch' },
+        { type: 'refactor', release: 'patch' },
+        { type: 'style', release: 'patch' },
+      ],
+    }],
     '@semantic-release/release-notes-generator',
     '@semantic-release/git',
     [
