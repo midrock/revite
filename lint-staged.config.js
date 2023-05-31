b8231dcb0bfa054ac0907f9971085436a739bea0
Roman Tebenkov <iteam4u@ya.ru>
2023-05-31 09:07:50 +0300

    chore: configure linters

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,11 +1,10 @@
 module.exports = {
   'src/**/*.ts': () => 'tsc -p tsconfig.json --noEmit',
   'demo/**/*.{ts,vue}': () => 'vue-tsc -p tsconfig.json --noEmit',
-  '*.{js,ts}': [
+  '*.{js,ts,vue}': [
     'eslint --fix',
   ],
-  '*.vue': [
-    'eslint --fix',
-    'stylelint **/*.vue --fix',
+  '*.{md,json,html}': [
+    'prettier --write',
   ],
 }
