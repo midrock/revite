9f708418dcad8b15e2d81e783833f0c6f773bf6f
Roman Tebenkov <iteam4u@ya.ru>
2023-10-09 06:12:33 +0300

    chore: update demo

--- demo/src/providers/EventServiceProvider.ts
+++ demo/src/providers/EventServiceProvider.ts
@@ -1,11 +1,10 @@
 import { RegisterContext, ServiceProvider } from 'revite'
 import { NoteCreatedEvent } from '/~/events/NoteCreatedEvent'
-import { NoteCreatedNotify } from '/~/listeners/NoteCreatedNotify'
 
 export class EventServiceProvider extends ServiceProvider {
   register(ctx: RegisterContext) {
     ctx.on(NoteCreatedEvent, [
-      NoteCreatedNotify,
+      () => import('/~/listeners/NoteCreatedNotify'),
     ])
   }
 }
--- demo/src/services/dashboard/DashboardServiceProvider.ts
+++ demo/src/services/dashboard/DashboardServiceProvider.ts
@@ -3,10 +3,13 @@ import { DashboardService } from '/~/services/dashboard'
 
 export class DashboardServiceProvider extends ServiceProvider {
   register(ctx: RegisterContext) {
-    ctx.bind(DashboardService).to({
+    ctx.bind(DashboardService).to<Service.Dashboard.Config>({
       config: 'dashboard',
       reactive: true,
       singleton: true,
+      factory({ Service }) {
+        return () => new Service()
+      },
     })
   }
 }
